aaeb433b011e7a4bc838d2e8b5a5d31a00cb54e9
deepakranjan <deepak.ranjan@sinilabs.co.in>
2018-06-18 12:05:44 +0530

    Added New Plugins

--- gruntfile.js
+++ gruntfile.js
@@ -4,9 +4,20 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("dd-mm-yyyy") %>\n' +
+      '* http://<%= pkg.homepage %>/\n' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
+      '<%= pkg.author.name %>; Licensed MIT */',
+
+    clean: {
+      dest: ['dest', 'dest2']
+    },
 
     copy: {
       t1: {
@@ -15,26 +26,40 @@ module.exports = function(grunt) {
       }
     },
 
+    concat: {
+      index: {
+        options: {
+          process: true
+        },
+        src: ['src/index.html'],
+        dest: 'public/index.html'
+      }
+    },
+
     // uglify: {
-    //   target1: {
+    //   build: {
     //     options: {
-    //       sourceMap: true
+    //       sourceMap: false,
+    //       banner: '<%= banner %>'
     //     },
     //     files: {
     //       'dest/output.min.js': ['src/app.js', 'src/one.js', 'src/t/two.js']
     //     }
     //   }
-    // }
+    // },
 
     uglify: {
-      main: {
+      build: {
+        options: {
+          banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        },
         files: [{
           cwd: 'src/',
           src: '**/*.js',
-          dest: 'dest2/',
+          dest: 'public/<%= pkg.version %>',
+          ext: '.min.js',
           expand: true,
-          flatten: false,
-          ext: '.min.js'
+          flatten: false
         }]
       }
     },
